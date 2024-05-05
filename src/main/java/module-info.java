a5b0d8122dc6d4359578e06590f64a7f017b7531
Pedro Coelho <pcdr@cesar.school>
2024-05-05 13:57:51 -0300

    javafx setup

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module com.warehouse.warehouse {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+
+    opens com.warehouse.warehouse to javafx.fxml;
+    exports com.warehouse.warehouse;
+}
