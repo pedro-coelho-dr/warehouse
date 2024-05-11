b13cf5108f69b657c7028c473f11d7c1519525d0
Kal <hiratelli.0@gmail.com>
2024-05-11 17:38:10 -0300

    adicionando entidade pessoa e iniciando DAO

---
+++ src/main/java/com/warehouse/warehouse/entity/Pessoa.java
@@ -0,0 +1,99 @@
+package com.warehouse.warehouse.entity;
+
+import java.time.LocalDateTime;
+
+public class Pessoa {
+    private int id;
+    private String nome;
+    private String email;
+    private String tipo;
+    private String cpf;
+    private String razao_social;
+    private String cnpj;
+    private String inscricao_estadual;
+    private LocalDateTime created_at;
+    private LocalDateTime updated_at;
+
+    public Pessoa(String nome, String email, String tipo, String cpf) {
+        this.nome = nome;
+        this.email = email;
+        this.tipo = tipo;
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRazao_social() {
+        return razao_social;
+    }
+
+    public void setRazao_social(String razao_social) {
+        this.razao_social = razao_social;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscricao_estadual() {
+        return inscricao_estadual;
+    }
+
+    public void setInscricao_estadual(String inscricao_estadual) {
+        this.inscricao_estadual = inscricao_estadual;
+    }
+
+    public LocalDateTime getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(LocalDateTime created_at) {
+        this.created_at = created_at;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
+}
---
+++ src/main/java/com/warehouse/warehouse/test.java
@@ -0,0 +1,54 @@
+package com.warehouse.warehouse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import com.warehouse.warehouse.entity.Pessoa;
+
+public class test {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/warehouse";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin";
+
+    public static void main(String[] args) {
+
+        try {
+//            conecta com o bd
+            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            String sql = "select id, nome, email, tipo, cpf from pessoa;";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String nome = resultSet.getString("nome");
+                String email = resultSet.getString("email");
+                String tipo = resultSet.getString("tipo");
+                String cpf = resultSet.getString("cpf");
+
+                Pessoa p1 = new Pessoa(nome, email, tipo, cpf);
+
+                System.out.println(id + " - " + p1.getNome() + " - " + p1.getEmail() + " - " + p1.getTipo() + " - " + p1.getCpf() + "\n");
+            }
+
+
+            // Example: Insert operation
+            //        insertData(connection, "John", 30);
+            // Example: Select operation
+            //        selectData(connection);
+            // Example: Update operation
+            //        updateData(connection, 1, "David");
+            // Example: Delete operation
+            //        deleteData(connection, 2);
+
+
+//            fecha conexao com o bd
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
