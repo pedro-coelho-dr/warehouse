5117dede947cc3bca34f96fd01d5422d58ebf13a
Diogo Henrique de Melo Correia <diogohenriquedemelo28@gmail.com>
2024-05-13 23:38:24 -0300

    Add FuncionarioView Completo

--- src/main/java/com/warehouse/warehouse/controller/FuncionarioAdicionarController.java
+++ src/main/java/com/warehouse/warehouse/controller/FuncionarioAdicionarController.java
@@ -1,25 +1,74 @@
 package com.warehouse.warehouse.controller;
 
+import com.warehouse.warehouse.database.DatabaseConnector;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.scene.Node;
+
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class FuncionarioAdicionarController {
 
     @FXML private Label titleLabel;
     @FXML private TextField emailField;
-    @FXML private ChoiceBox<String> departamentoChoiceBox;
     @FXML private TextField nomeField;
     @FXML private TextField cpfField;
+
+    @FXML private ComboBox<String> departamentoChoiceBox;
+
     @FXML private TextField salarioField;
     @FXML private DatePicker dtContratacaoDatePicker;
+
+
+
+    // Telefone
+    @FXML private VBox phoneContainer;
+
+    // Endereço
+    @FXML private TextField ruaField;
+    @FXML private TextField numeroField;
+    @FXML private TextField bairroField;
+    @FXML private TextField cidadeField;
+    @FXML private ComboBox<String> estadoComboBox;
+    @FXML private TextField cepField;
+
+    @FXML private Label statusLabel;
+    private final List<String> departamentosTest = Arrays.asList(
+            "1", "2"
+    );
+
+    private final List<String> estados = Arrays.asList(
+            "AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
+            "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+            "RS", "RO", "RR", "SC", "SP", "SE", "TO"
+    );
 
     @FXML
     private void initialize() {
         titleLabel.setText("Adicionar Funcionário");
-        departamentoChoiceBox.getItems().addAll("1", "2");
+        departamentoChoiceBox.setItems(FXCollections.observableArrayList(departamentosTest));
+        estadoComboBox.setItems(FXCollections.observableArrayList(estados));
     }
 
+    @FXML
+    private void addPhoneField() {
+        TextField newPhoneField = new TextField();
+        newPhoneField.setMaxWidth(300);
+        newPhoneField.setPromptText("Telefone");
+        phoneContainer.getChildren().add(newPhoneField);
+    }
 
+    @FXML
+    private void removePhoneField() {
+        int count = phoneContainer.getChildren().size();
+        if (count > 1) {
+            phoneContainer.getChildren().remove(count - 1);
+        }
+    }
 
 
 }
